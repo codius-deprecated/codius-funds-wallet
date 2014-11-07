a5a85fa5c59bcfeb4116e6910a5b70fd68842bcb
Evan Schwartz <evan.mark.schwartz@gmail.com>
2014-11-07 14:28:28 -0800

    [TASK] Use test-client to fund accounts

--- test-client.js
+++ test-client.js
@@ -3,16 +3,17 @@ var nacl = require('tweetnacl');
 var httpSignature = require('http-signature');
 var request = Promise.promisify(require('request'));
 
+var UID_TO_FUND = 'xiW7zebyjI51kBGNeJaehA';
 
 var keypair1 = nacl.sign.keyPair();
 var keypair2 = nacl.sign.keyPair();
 
 var user1 = {
-	user_id: 'user1',
+	user_id: 'god' + (Math.random()),
 	public_key: nacl.util.encodeBase64(keypair1.publicKey),
 	balances: [{
 		currency: 'XRP',
-		balance: 10000000000
+		balance: 150000
 	}]
 };
 var user2 = {
@@ -21,58 +22,58 @@ var user2 = {
 };
 
 
-function sendPayment(amount){
-	var payment = {
-		payment_id: 'test',
-		source: 'user1',
-		destination: 'user2',
-		currency: 'XRP',
-		amount: amount
-	};
-	return request({
-		url: 'http://localhost:8000/payments',
-		method: 'POST',
-		json: payment,
-		httpSignature: {
-			keyId: user1.user_id,
-			key: nacl.util.encodeBase64(keypair1.secretKey),
-			algorithm: 'ed25519-sha512'
-		}
-	})
-}
+// function sendPayment(amount){
+// 	var payment = {
+// 		payment_id: 'test',
+// 		source: 'user1',
+// 		destination: 'user2',
+// 		currency: 'XRP',
+// 		amount: amount
+// 	};
+// 	return request({
+// 		url: 'https://5gp9j6-d3rx2q-25bc9x.codius.host/payments',
+// 		method: 'POST',
+// 		json: payment,
+// 		httpSignature: {
+// 			keyId: user1.user_id,
+// 			key: nacl.util.encodeBase64(keypair1.secretKey),
+// 			algorithm: 'ed25519-sha512'
+// 		}
+// 	})
+// }
 
-function sendPaymentAndCheckBalances() {
-	return sendPayment(100)
-	.then(function(){
-		return request({
-			url: 'http://localhost:8000/users/user1',
-			httpSignature: {
-				keyId: user1.user_id,
-				key: nacl.util.encodeBase64(keypair1.secretKey),
-				algorithm: 'ed25519-sha512'
-			}
-		});
-	})
-	.then(function(requestResponse){
-		console.log('After payment this is User 1', JSON.stringify(requestResponse[1], null, 2));
-	})
-	.then(function(){
-		return request({
-			url: 'http://localhost:8000/users/user2',
-			httpSignature: {
-				keyId: user2.user_id,
-				key: nacl.util.encodeBase64(keypair2.secretKey),
-				algorithm: 'ed25519-sha512'
-			}
-		});
-	})
-	.then(function(requestResponse){
-		console.log('After payment this is User 2', JSON.stringify(requestResponse[1], null, 2));
-	})
-}
+// function sendPaymentAndCheckBalances() {
+// 	return sendPayment(100)
+// 	.then(function(){
+// 		return request({
+// 			url: 'https://5gp9j6-d3rx2q-25bc9x.codius.host/users/user1',
+// 			httpSignature: {
+// 				keyId: user1.user_id,
+// 				key: nacl.util.encodeBase64(keypair1.secretKey),
+// 				algorithm: 'ed25519-sha512'
+// 			}
+// 		});
+// 	})
+// 	.then(function(requestResponse){
+// 		console.log('After payment this is User 1', JSON.stringify(requestResponse[1], null, 2));
+// 	})
+// 	.then(function(){
+// 		return request({
+// 			url: 'https://5gp9j6-d3rx2q-25bc9x.codius.host/users/user2',
+// 			httpSignature: {
+// 				keyId: user2.user_id,
+// 				key: nacl.util.encodeBase64(keypair2.secretKey),
+// 				algorithm: 'ed25519-sha512'
+// 			}
+// 		});
+// 	})
+// 	.then(function(requestResponse){
+// 		console.log('After payment this is User 2', JSON.stringify(requestResponse[1], null, 2));
+// 	})
+// }
 
 request({
-	url: 'http://localhost:8000/showmethemoney',
+	url: 'https://5gp9j6-d3rx2q-25bc9x.codius.host/showmethemoney',
 	method: 'POST',
 	json: user1,
 	httpSignature: {
@@ -85,37 +86,77 @@ request({
 	console.log('Registered User 1', JSON.stringify(requestResponse[1], null, 2));
 })
 .then(function(){
+		var payment = {
+		payment_id: 'test',
+		source: user1.user_id,
+		destination: UID_TO_FUND,
+		currency: 'XRP',
+		amount: 15002
+	};
 	return request({
-		url: 'http://localhost:8000/users',
+		url: 'https://5gp9j6-d3rx2q-25bc9x.codius.host/payments',
 		method: 'POST',
-		json: user2,
+		json: payment,
 		httpSignature: {
-			keyId: user2.user_id,
-			key: nacl.util.encodeBase64(keypair2.secretKey),
+			keyId: user1.user_id,
+			key: nacl.util.encodeBase64(keypair1.secretKey),
 			algorithm: 'ed25519-sha512'
 		}
 	})
 })
 .then(function(requestResponse){
-	console.log('Registered User 2', JSON.stringify(requestResponse[1], null, 2));
+		console.log(JSON.stringify(requestResponse[1], null, 2));
+	})
+	.then(function(){
+		return request({
+			url: 'https://5gp9j6-d3rx2q-25bc9x.codius.host/users/' + user1.user_id,
+			httpSignature: {
+				keyId: user1.user_id,
+				key: nacl.util.encodeBase64(keypair1.secretKey),
+				algorithm: 'ed25519-sha512'
+			}
+		});
+	})
+.then(function(requestResponse){
+	console.log('After payment user 1 is:', JSON.stringify(requestResponse[1], null, 2));
 })
-// .then(sendPaymentAndCheckBalances)
-// .then(sendPaymentAndCheckBalances)
-// .then(sendPaymentAndCheckBalances)
-// .then(sendPaymentAndCheckBalances)
-// .then(sendPaymentAndCheckBalances)
-// .then(sendPaymentAndCheckBalances)
-
-
-.then(function(){
-	var numbers = [];
-	for (var i = 0; i < 100; i++) {
-		numbers.push(i);
-	}
-
-	return Promise.each(numbers, sendPaymentAndCheckBalances);
-})
-
 .catch(function(error){
-	console.error(error);
-});
+	console.log(error);
+})
+// .then(function(){
+// 	return request({
+// 		url: 'https://5gp9j6-d3rx2q-25bc9x.codius.host/users',
+// 		method: 'POST',
+// 		json: user2,
+// 		httpSignature: {
+// 			keyId: user2.user_id,
+// 			key: nacl.util.encodeBase64(keypair2.secretKey),
+// 			algorithm: 'ed25519-sha512'
+// 		}
+// 	})
+// })
+// .then(function(requestResponse){
+// 	console.log('Registered User 2', JSON.stringify(requestResponse[1], null, 2));
+// })
+// // .then(sendPaymentAndCheckBalances)
+// // .then(sendPaymentAndCheckBalances)
+// // .then(sendPaymentAndCheckBalances)
+// // .then(sendPaymentAndCheckBalances)
+// // .then(sendPaymentAndCheckBalances)
+// // .then(sendPaymentAndCheckBalances)
+
+
+// .then(function(){
+// 	var numbers = [];
+// 	for (var i = 0; i < 100; i++) {
+// 		numbers.push(i);
+// 	}
+
+// 	return Promise.each(numbers, sendPaymentAndCheckBalances);
+// })
+
+// .catch(function(error){
+// 	console.error(error);
+// });
+
+
